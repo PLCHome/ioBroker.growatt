330efdb37f80f1c10df24b8ad15d748c879245e3
ChrisHanuta <GitHub@traeger-welt.de>
2024-08-16 12:51:14 +0200

    after lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,56 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/admin", "**/test", "lib/env/tools.js", "**/gulpfile.js", "eslint.config.mjs"],
-}, ...compat.extends("eslint:recommended", "plugin:prettier/recommended"), {
+export default [
+  {
+    ignores: ['**/admin', '**/test', 'lib/env/tools.js', '**/gulpfile.js', 'eslint.config.mjs'],
+  },
+  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  {
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            ...globals.mocha,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha,
+      },
 
-        ecmaVersion: 2018,
-        sourceType: "module",
+      ecmaVersion: 2018,
+      sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                impliedStrict: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          impliedStrict: true,
         },
+      },
     },
 
     rules: {
-        "no-use-before-define": ["error", {
-            functions: false,
-        }],
+      'no-use-before-define': [
+        'error',
+        {
+          functions: false,
+        },
+      ],
 
-        "no-continue": "off",
+      'no-continue': 'off',
 
-        "no-param-reassign": ["error", {
-            props: false,
-        }],
+      'no-param-reassign': [
+        'error',
+        {
+          props: false,
+        },
+      ],
     },
-}];
+  },
+];
